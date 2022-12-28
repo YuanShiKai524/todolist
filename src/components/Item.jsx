656e77a3093922f260a297b6475172601c65c0da
yuanshikai524 <gcobc04992@gmail.com>
2022-12-28 21:53:10 +0800

    新增了List跟Item組件，把大致排版也做出來了，但每個Item組件裡的東西尚未完成，接著要做。

---
+++ src/components/Item.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Item = () => {
+  return (
+    <ul>
+      <li className='item'>Item</li>
+      <li className='item'>Item</li>
+      <li className='item'>Item</li>
+      <li className='item'>Item</li>
+      <li className='item'>Item</li>
+      <li className='item'>Item</li>
+    </ul>
+  )
+}
+
+export default Item
---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Item from './Item'
+
+const List = () => {
+  return (
+    <div className='todo-list-container'>
+      <Item />
+    </div>
+  )
+}
+
+export default List
