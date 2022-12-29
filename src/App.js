f73161c222c465fcf4dd9176cdb63dce45e81514
yuanshikai524 <gcobc04992@gmail.com>
2022-12-29 23:13:43 +0800

    完成了move done to end的結構與樣式 (MoveDoneToEnd組件)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Main from './components/Main';
+import MoveDoneToEnd from "./components/MoveDoneToEnd";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Main />
+      <MoveDoneToEnd />
     </div>
   );
 }
---
+++ src/components/MoveDoneToEnd.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MoveDoneToEnd = () => {
+  return (
+    <div className='move-to-end-container'>
+      <h6>Move done things to end?</h6>
+      <div className='switch-wrapper'>
+        <div className='switch'></div>
+      </div>
+    </div>
+  )
+}
+
+export default MoveDoneToEnd
